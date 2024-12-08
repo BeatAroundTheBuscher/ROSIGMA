d87333aa61f9ca327a576d237109ec8667f7556b
Buscher <beataroundthebuscher@protonmail.com>
2024-12-08 22:16:27 +0100

    Show only the first 7 bytes (similar how github does it)

--- metadata_creator.sh
+++ metadata_creator.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 set -x # Enable command tracing to show executed commands
-cat metadata_template.yml | sed "s/version: \"/version: \"$1 - /"
+git_id=$(echo $1 | cut -b 1-7)
+cat metadata_template.yml | sed "s/version: \"/version: \"$git_id - /"
