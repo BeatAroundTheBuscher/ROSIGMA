fee41b2c37e0cc2d96e5a339f0c63804d59f65dc
Buscher <beataroundthebuscher@protonmail.com>
2024-12-22 18:29:02 +0100

    Linter Phase 2

--- metadata_creator.sh
+++ metadata_creator.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-set -x # Enable command tracing to show executed commands
+set -x  # Enable command tracing to show executed commands
 git_id=$(echo $1 | cut -b 1-7)
 cat metadata_template.yml | sed "s/version: \"/version: \"$git_id - /"
